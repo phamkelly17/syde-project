e92c61d85a305bf850f5e953b710d2b5f6685a70
Kelly Pham <86501308+phamkelly17@users.noreply.github.com>
2022-04-15 17:06:54 -0400

    adding Utils directory

---
+++ SydeProject/Utils/ImagePicker.swift
@@ -0,0 +1,41 @@
+//
+//  ImagePicker.swift
+//  SydeProject_4.0
+//
+//  Created by Kelly Pham on 2022-02-25.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var selectedImage: UIImage?
+    @Environment(\.presentationMode) var thisPresenationMode
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+        
+    }
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context){
+        
+    }
+}
+
+extension ImagePicker {
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+        init(_ parent: ImagePicker){
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            guard let image = info[.originalImage] as? UIImage else { return }
+            parent.selectedImage = image
+            parent.thisPresenationMode.wrappedValue.dismiss()
+        }
+    }
+}
